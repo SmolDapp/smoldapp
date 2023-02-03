f8caa123bc07fe366e50f220e2d116d5ca5ae42e
Majorfi <major-tom.eth@pm.me>
2023-02-03 12:06:23 +0100

    feat: ledger hide not supported networks

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -4,12 +4,14 @@ import {useRouter} from 'next/router';
 import Logo from 'components/icons/logo';
 import Drawer from 'components/SettingsDrawer';
 import {useMenu} from 'contexts/useMenu';
+import {useWeb3} from '@yearn-finance/web-lib/contexts/useWeb3';
 import IconSettings from '@yearn-finance/web-lib/icons/IconSettings';
 import Header from '@yearn-finance/web-lib/layouts/Header.next';
 
 import type {ReactElement} from 'react';
 
 function	AppHeader(): ReactElement {
+	const	{walletType} = useWeb3();
 	const	{pathname} = useRouter();
 	const	{onOpenMenu} = useMenu();
 	const	[isDrawerOpen, set_isDrawerOpen] = useState(false);
@@ -21,7 +23,7 @@ function	AppHeader(): ReactElement {
 				currentPathName={pathname}
 				onOpenMenuMobile={onOpenMenu}
 				nav={[{path: '/', label: <Logo className={'h-8 text-neutral-900'} />}]}
-				supportedNetworks={[1, 10, 250, 42161]}
+				supportedNetworks={walletType === 'EMBED_LEDGER' ? [1] : [1, 10, 250, 42161]}
 				logo={(<div />)}
 				extra={(
 					<div className={'ml-4 flex'}>
