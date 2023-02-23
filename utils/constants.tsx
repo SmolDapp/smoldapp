111c72a01aa44fe6da8b88559d4299a5655ec6a4
Majorfi <major-tom.eth@pm.me>
2023-02-23 11:21:33 +0100

    fix: add missing constant

---
+++ utils/constants.tsx
@@ -0,0 +1,5 @@
+import {toAddress} from '@yearn-finance/web-lib/utils/address';
+
+const MATIC_TOKEN_ADDRESS = toAddress('0x0000000000000000000000000000000000001010');
+
+export {MATIC_TOKEN_ADDRESS};
