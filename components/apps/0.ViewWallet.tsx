3027b4b9b04ecd2a190c533dfa0ddf728cf80d7d
Major <major@ycorpo.com>
2023-09-03 12:18:33 +0200

    feat: rainbow clean

--- components/apps/0.ViewWallet.tsx
+++ components/apps/0.ViewWallet.tsx
@@ -1,11 +1,7 @@
-import React, {Fragment, useCallback, useMemo} from 'react';
+import React, {Fragment} from 'react';
 import CardWithIcon from 'components/common/CardWithIcon';
-import {useWallet} from 'contexts/useWallet';
 import {useIsMounted} from '@react-hookz/web';
 import {useWeb3} from '@yearn-finance/web-lib/contexts/useWeb3';
-import {useInjectedWallet} from '@yearn-finance/web-lib/hooks/useInjectedWallet';
-import IconWalletLedger from '@yearn-finance/web-lib/icons/IconWalletLedger';
-import IconWalletSafe from '@yearn-finance/web-lib/icons/IconWalletSafe';
 import IconWalletWalletConnect from '@yearn-finance/web-lib/icons/IconWalletWalletConnect';
 import ViewSectionHeading from '@common/ViewSectionHeading';
 
@@ -16,39 +12,7 @@ type TViewWalletProps = {
 };
 
 function SectionWalletSelection({onSelect}: TViewWalletProps): ReactElement {
-	const {onConnect, walletType} = useWeb3();
-	const detectedWalletProvider = useInjectedWallet();
-	const {walletProvider, set_walletProvider} = useWallet();
-
-	const onSelectWallet = useCallback(async (walletType: string): Promise<void> => {
-		try {
-			set_walletProvider(walletType);
-			onSelect();
-			await onConnect(walletType, (): void => set_walletProvider('NONE'), (): void => undefined);
-		} catch {
-			//
-		}
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [onConnect]);
-
-	const detectedWallet = useMemo((): {name: string, icon: ReactElement} => {
-		if (walletType === 'EMBED_LEDGER') {
-			return ({
-				name: 'Ledger',
-				icon: <IconWalletLedger />
-			});
-		}
-		if (walletType === 'EMBED_GNOSIS_SAFE') {
-			return ({
-				name: 'Safe',
-				icon: <IconWalletSafe />
-			});
-		}
-		return ({
-			name: detectedWalletProvider.name,
-			icon: detectedWalletProvider.icon
-		});
-	}, [detectedWalletProvider, walletType]);
+	const {isActive, onConnect} = useWeb3();
 
 	const isMounted = useIsMounted();
 	if (!isMounted()) {
@@ -57,19 +21,15 @@ function SectionWalletSelection({onSelect}: TViewWalletProps): ReactElement {
 
 	return (
 		<Fragment>
-			<div className={'relative col-span-6 md:col-span-4'}>
+			<div className={`relative col-span-6 md:col-span-4 flex`}>
 				<CardWithIcon
-					isSelected={walletProvider === 'INJECTED' || walletType === 'INJECTED' || ['EMBED_LEDGER', 'EMBED_GNOSIS_SAFE'].includes(walletType)}
-					icon={detectedWallet.icon}
-					label={detectedWallet.name}
-					onClick={async (): Promise<void> => ['EMBED_LEDGER', 'EMBED_GNOSIS_SAFE'].includes(walletType) ? undefined : onSelectWallet('INJECTED')} />
-			</div>
-			<div className={`relative col-span-6 md:col-span-4 ${['EMBED_LEDGER', 'EMBED_GNOSIS_SAFE'].includes(walletType) ? 'hidden' : 'flex'}`}>
-				<CardWithIcon
-					isSelected={walletProvider === 'WALLET_CONNECT' || walletType === 'WALLET_CONNECT'}
+					isSelected={isActive}
 					icon={<IconWalletWalletConnect />}
 					label={'WalletConnect'}
-					onClick={async (): Promise<void> => onSelectWallet('WALLET_CONNECT')} />
+					onClick={async (): Promise<void> => {
+						await onConnect()
+						onSelect();
+					}} />
 			</div>
 		</Fragment>
 	);
