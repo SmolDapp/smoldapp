05b23a93a902a48124bdb5661d7a05e4ed4d0d65
Daniil Polienko <donyaschefer@gmail.com>
2023-12-05 20:05:42 +0300

    feat: add hover to download buttons

--- components/apps/tokenlistooor/DownloadAssetButton.tsx
+++ components/apps/tokenlistooor/DownloadAssetButton.tsx
@@ -7,7 +7,9 @@ import type {TDownloadAsset} from '@utils/downloadAsset';
 export function DownloadAssetButton({address, type, chainId, fileName}: TDownloadAsset): ReactElement {
 	return (
 		<button
-			className={'flex items-center gap-1 rounded-full bg-neutral-100 px-2 py-1'}
+			className={
+				'flex items-center gap-1 rounded-full bg-neutral-100 px-2 py-1 transition-colors hover:bg-neutral-200'
+			}
 			onClick={() =>
 				downloadAsset({
 					address,
