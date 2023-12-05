6aa0c8d0cae710e6cde3840037a8955f72acb8b8
Majorfi <major-tom.eth@pm.me>
2023-12-05 18:52:20 +0100

    fix: not supported chain

--- components/common/ViewSectionHeading.tsx
+++ components/common/ViewSectionHeading.tsx
@@ -13,8 +13,12 @@ function ViewSectionHeading({title, content, configSection, className}: TViewSec
 	return (
 		<div className={cl('relative col-span-12 flex flex-col p-4 text-neutral-900 md:p-6', className)}>
 			<div className={'w-full md:w-3/4'}>
-				<b>{title}</b>
-				<p className={'whitespace-pre-wrap text-sm text-neutral-500'}>{content}</p>
+				<b suppressHydrationWarning>{title}</b>
+				<p
+					suppressHydrationWarning
+					className={'whitespace-pre-wrap text-sm text-neutral-500'}>
+					{content}
+				</p>
 			</div>
 			{configSection ? (
 				<div className={'absolute right-2 top-2 flex flex-col space-y-4 md:right-4 md:top-4'}>
