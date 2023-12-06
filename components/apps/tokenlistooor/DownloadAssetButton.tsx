c920ff52a0db8d5cb0c0fd0dc65178ab8e47aa4f
Daniil Polienko <donyaschefer@gmail.com>
2023-12-06 17:38:56 +0300

    fix: remove file-saver lib to be able to handle errors manually

--- components/apps/tokenlistooor/DownloadAssetButton.tsx
+++ components/apps/tokenlistooor/DownloadAssetButton.tsx
@@ -1,24 +1,51 @@
+import {useDownloadFile} from 'hooks/useDownloadFile';
+import axios from 'axios';
 import IconDownload from '@icons/IconDownload';
-import {downloadAsset} from '@utils/downloadAsset';
+import {toast} from '@yearn-finance/web-lib/components/yToast';
 
+import type {AxiosResponse} from 'axios';
 import type {ReactElement} from 'react';
-import type {TDownloadAsset} from '@utils/downloadAsset';
+import type {TAddress} from '@yearn-finance/web-lib/types';
 
-export function DownloadAssetButton({address, type, chainId, fileName, onSuccess}: TDownloadAsset): ReactElement {
+export type TDownloadAssetButtonProps = {
+	chainId: number;
+	address: TAddress;
+	type: 'png' | 'svg';
+	onSuccess?: () => void;
+	fileName?: string;
+};
+
+export function DownloadAssetButton({
+	address,
+	type,
+	chainId,
+	fileName,
+	onSuccess
+}: TDownloadAssetButtonProps): ReactElement {
+	const downloadFile = async (): Promise<AxiosResponse<Blob>> => {
+		const url = `https://raw.githubusercontent.com/SmolDapp/tokenAssets/main/tokens/${chainId}/${address.toLowerCase()}/${
+			type === 'png' ? 'logo-128.png' : 'logo.svg'
+		}`;
+
+		return axios.get(url, {
+			responseType: 'blob'
+		});
+	};
+
+	const {download} = useDownloadFile({
+		apiDefinition: downloadFile,
+		postDownloading: onSuccess,
+		onError: () => toast({type: 'error', content: 'Something went wrong'}),
+		fileName,
+		fileType: type
+	});
+
 	return (
 		<button
 			className={
 				'flex items-center gap-1 rounded-full bg-neutral-100 px-2 py-1 transition-colors hover:bg-neutral-200'
 			}
-			onClick={() =>
-				downloadAsset({
-					address,
-					type,
-					chainId,
-					fileName,
-					onSuccess
-				})
-			}>
+			onClick={download}>
 			{type.toUpperCase()}
 			<IconDownload />
 		</button>
