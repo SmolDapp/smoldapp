94eb6c8c3fbef7ea1519a47f6f6f9d0709580412
Major <major@ycorpo.com>
2023-12-08 11:05:53 +0100

    fix: remove extra components from navProfile preview

--- pages/components/Profile.tsx
+++ pages/components/Profile.tsx
@@ -1,7 +1,4 @@
 import {ProfileAddress, ProfileAvatar} from 'components/designSystem/Profile';
-import {IconChevron} from '@icons/IconChevron';
-import {cl} from '@yearn-finance/web-lib/utils/cl';
-import {getNetwork} from '@yearn-finance/web-lib/utils/wagmi/utils';
 
 import type {ReactElement} from 'react';
 import type {TAddress} from '@yearn-finance/web-lib/types';
@@ -14,40 +11,18 @@ type TNavProfileDemoProps = {
 	avatar?: string;
 };
 function NavProfile(props: TNavProfileDemoProps): ReactElement {
-	const currentChain = getNetwork(1).nativeCurrency;
-
 	return (
-		<section className={'p-4'}>
-			<div className={'flex gap-2'}>
-				<ProfileAvatar
-					isLoading={props.isLoadingAvatar}
-					src={props.avatar}
-				/>
-				<ProfileAddress
-					isConnecting={props.isConnecting}
-					address={props.address}
-					ens={props.ens}
-				/>
-			</div>
-			<hr className={'mb-2 mt-4 text-neutral-50'} />
-			<div className={'grid grid-cols-2 gap-6'}>
-				<div>
-					<small className={'text-xxs'}>{'Chain'}</small>
-					<div
-						className={cl(
-							'flex w-full items-center justify-between rounded-lg bg-neutral-50 p-2',
-							'text-xs'
-						)}>
-						{currentChain.name || 'Ethereum'}
-						<IconChevron className={'h-4 w-4 rotate-90 text-neutral-900'} />
-					</div>
-				</div>
-				<div>
-					<small className={'text-xxs'}>{currentChain.symbol || 'ETH'}</small>
-					<strong className={'text-base leading-8'}>{'3.692345'}</strong>
-				</div>
-			</div>
-		</section>
+		<div className={'flex gap-2'}>
+			<ProfileAvatar
+				isLoading={props.isLoadingAvatar}
+				src={props.avatar}
+			/>
+			<ProfileAddress
+				isConnecting={props.isConnecting}
+				address={props.address}
+				ens={props.ens}
+			/>
+		</div>
 	);
 }
 
@@ -57,7 +32,7 @@ export default function Component(): ReactElement {
 			<div className={'flex h-fit w-full max-w-6xl flex-wrap items-center justify-center gap-4'}>
 				<div>
 					<small className={'pb-1'}>{'Loading'}</small>
-					<div className={'flex w-[280px] flex-col rounded-lg bg-neutral-0'}>
+					<div className={'flex w-[280px] flex-col rounded-lg bg-neutral-0 p-4'}>
 						<NavProfile
 							isLoadingAvatar
 							isConnecting
@@ -67,7 +42,7 @@ export default function Component(): ReactElement {
 
 				<div>
 					<small className={'pb-1'}>{'Loading avatar, no ens'}</small>
-					<div className={'flex w-[280px] flex-col rounded-lg bg-neutral-0'}>
+					<div className={'flex w-[280px] flex-col rounded-lg bg-neutral-0 p-4'}>
 						<NavProfile
 							isLoadingAvatar
 							isConnecting={false}
@@ -80,7 +55,7 @@ export default function Component(): ReactElement {
 
 				<div>
 					<small className={'pb-1'}>{'Ens, no avatar'}</small>
-					<div className={'flex w-[280px] flex-col rounded-lg bg-neutral-0'}>
+					<div className={'flex w-[280px] flex-col rounded-lg bg-neutral-0 p-4'}>
 						<NavProfile
 							isLoadingAvatar={false}
 							isConnecting={false}
@@ -93,7 +68,7 @@ export default function Component(): ReactElement {
 
 				<div>
 					<small className={'pb-1'}>{'Ens, avatar'}</small>
-					<div className={'flex w-[280px] flex-col rounded-lg bg-neutral-0'}>
+					<div className={'flex w-[280px] flex-col rounded-lg bg-neutral-0 p-4'}>
 						<NavProfile
 							isLoadingAvatar={false}
 							isConnecting={false}
