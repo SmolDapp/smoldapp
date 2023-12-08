da3d831a35c7d62ead761cba107505b6a88720a4
Major <major@ycorpo.com>
2023-12-08 15:30:12 +0100

    style: force width

--- pages/components/AddressInput.tsx
+++ pages/components/AddressInput.tsx
@@ -6,7 +6,9 @@ export default function AddressInputView(): ReactElement {
 	return (
 		<div className={'fixed inset-0 flex items-center justify-center'}>
 			<div className={'flex items-center justify-center rounded-lg bg-neutral-0 p-4'}>
-				<SmolAddressInput />
+				<div className={'w-[442px]'}>
+					<SmolAddressInput />
+				</div>
 			</div>
 		</div>
 	);
