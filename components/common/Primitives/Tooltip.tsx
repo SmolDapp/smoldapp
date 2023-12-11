61cc8156c592438793719647f6ed52ec28e91d82
Major <90963895+Majorfi@users.noreply.github.com>
2023-12-11 18:55:47 +0100

    Update components/common/Primitives/Tooltip.tsx

--- components/common/Primitives/Tooltip.tsx
+++ components/common/Primitives/Tooltip.tsx
@@ -13,7 +13,6 @@ export const TooltipContent = forwardRef<
 	<TooltipPrimitive.Content
 		ref={ref}
 		sideOffset={sideOffset}
-		// style={{boxShadow: 'rgba(36, 40, 51, 0.08) 0px 0px 20px 8px'}}
 		className={cl(
 			'z-50 overflow-hidden rounded-md px-2 py-1.5',
 			'animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
