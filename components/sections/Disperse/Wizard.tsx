20f6110c5173c1efecb2fba412725b4c7899c8a0
Daniil Polienko <donyaschefer@gmail.com>
2024-03-12 18:14:31 +0300

    feat: add simple total to disperse label

--- components/sections/Disperse/Wizard.tsx
+++ components/sections/Disperse/Wizard.tsx
@@ -1,7 +1,4 @@
 import React, {useCallback, useMemo, useState} from 'react';
-import {IconCircleCheck} from 'components/icons/IconCircleCheck';
-import {IconCircleCross} from 'components/icons/IconCircleCross';
-import {IconSpinner} from 'components/icons/IconSpinner';
 import {Button} from 'components/Primitives/Button';
 import {approveERC20, disperseERC20, disperseETH} from 'utils/actions';
 import {notifyDisperse} from 'utils/notifier';
@@ -26,7 +23,6 @@ import type {BaseError, Hex} from 'viem';
 import type {TAddress} from '@builtbymom/web3/types';
 import type {TTxStatus} from '@builtbymom/web3/utils/wagmi/transaction';
 import type {BaseTransaction} from '@gnosis.pm/safe-apps-sdk';
-import type {TDisperseInput} from './useDisperse';
 
 type TApprovalWizardProps = {
 	onSuccess: () => void;
@@ -266,252 +262,6 @@ const useConfirmDisperse = ({
 	return {onDisperseTokens};
 };
 
-type TApprovalStatusProps = {
-	approvalStatus: TTxStatus;
-	allowance: bigint;
-	totalToDisperse: bigint;
-};
-
-function ApprovalStatus({approvalStatus, allowance, totalToDisperse}: TApprovalStatusProps): ReactElement {
-	const {configuration} = useDisperse();
-	const {getBalance} = useWallet();
-
-	function renderStatusIndicator(): ReactElement {
-		if (!configuration.tokenToSend) {
-			return <div className={'size-4 rounded-full border border-neutral-200 bg-neutral-300'} />;
-		}
-		// TODO: fix when totaltodiserse is 0 (values are not set)
-		if (allowance >= totalToDisperse) {
-			return <IconCircleCheck className={'size-4 text-green'} />;
-		}
-		if (approvalStatus.pending) {
-			return <IconSpinner className={'size-4'} />;
-		}
-		if (approvalStatus.success) {
-			return <IconCircleCheck className={'size-4 text-green'} />;
-		}
-		if (approvalStatus.error) {
-			return <IconCircleCross className={'size-4 text-red'} />;
-		}
-		if (
-			totalToDisperse >
-			getBalance({
-				address: toAddress(configuration.tokenToSend?.address),
-				chainID: configuration.tokenToSend.chainID
-			}).raw
-		) {
-			return <IconCircleCross className={'size-4 text-red'} />;
-		}
-
-		return <div className={'size-4 rounded-full border border-neutral-200 bg-neutral-300'} />;
-	}
-
-	function renderStatusMessage(): ReactElement {
-		if (totalToDisperse === 0n) {
-			return <div className={'text-left text-xs'}>{'You have nothing to approve '}</div>;
-		}
-		if (allowance >= totalToDisperse || approvalStatus.success) {
-			return (
-				<div className={'text-left text-xs'}>
-					{'You have already approved '}
-					<span
-						suppressHydrationWarning
-						className={'font-number font-bold'}>
-						{formatAmount(
-							toNormalizedValue(totalToDisperse, configuration.tokenToSend?.decimals || 18),
-							6,
-							configuration.tokenToSend?.decimals || 18
-						)}
-						{` ${configuration.tokenToSend?.symbol || 'Tokens'}`}
-					</span>
-				</div>
-			);
-		}
-
-		if (approvalStatus.pending) {
-			return (
-				<div className={'text-left text-xs'}>
-					{'Approving '}
-					<span
-						suppressHydrationWarning
-						className={'font-number font-bold'}>
-						{formatAmount(
-							toNormalizedValue(totalToDisperse, configuration.tokenToSend?.decimals || 18),
-							6,
-							configuration.tokenToSend?.decimals || 18
-						)}
-						{` ${configuration.tokenToSend?.symbol || 'Tokens'} ...`}
-					</span>
-				</div>
-			);
-		}
-		if (
-			totalToDisperse >
-			getBalance({
-				address: toAddress(configuration.tokenToSend?.address),
-				chainID: Number(configuration.tokenToSend?.chainID)
-			}).raw
-		) {
-			return (
-				<div className={'text-left text-xs'}>
-					{`You don't have enough ${configuration.tokenToSend?.symbol || 'Tokens'}`}
-				</div>
-			);
-		}
-
-		return (
-			<div className={'text-left text-xs'}>
-				{'You need to approve the spending of '}
-				<span
-					suppressHydrationWarning
-					className={'font-number font-bold'}>
-					{formatAmount(
-						toNormalizedValue(totalToDisperse, configuration.tokenToSend?.decimals || 18),
-						6,
-						configuration.tokenToSend?.decimals || 18
-					)}
-					{` ${configuration.tokenToSend?.symbol || 'Tokens'}`}
-				</span>
-			</div>
-		);
-	}
-
-	if (!configuration.tokenToSend) {
-		return (
-			<div className={'flex flex-col'}>
-				<div className={'font-bold'}>{'Info'}</div>
-				<div className={'text-left text-xs text-neutral-900/40'}>{'Please select a token'}</div>
-			</div>
-		);
-	}
-
-	return (
-		<div className={'flex flex-col'}>
-			<div className={'flex items-center gap-2'}>
-				<div className={'font-bold'}>{'Approve Summary'}</div>
-				{renderStatusIndicator()}
-			</div>
-			<div className={'text-left text-xs'}>
-				<div className={'flex w-full flex-col'}>
-					<div className={''}>
-						{'You have '}
-						<span
-							suppressHydrationWarning
-							className={'font-number font-bold'}>
-							{formatAmount(
-								getBalance({
-									address: toAddress(configuration.tokenToSend?.address),
-									chainID: Number(configuration.tokenToSend?.chainID)
-								}).normalized,
-								6,
-								configuration.tokenToSend?.decimals || 18
-							)}
-							{` ${configuration.tokenToSend?.symbol || 'Tokens'}`}
-						</span>
-					</div>
-					{renderStatusMessage()}
-				</div>
-			</div>
-		</div>
-	);
-}
-
-function NothingToDisperse(): ReactElement {
-	return (
-		<div className={'flex w-full flex-row items-center md:flex-row md:items-center'}>
-			<div className={'text-left text-xs text-neutral-900/40'}>
-				{'Please add some receivers to disperse tokens'}
-			</div>
-		</div>
-	);
-}
-
-function DisperseElement({row}: {row: TDisperseInput}): ReactElement {
-	const {configuration} = useDisperse();
-
-	return (
-		<div className={'flex w-full flex-row items-center md:flex-row md:items-center'}>
-			<div className={'text-left text-xs'}>
-				{'Sending '}
-				<span className={'font-number font-bold'}>
-					{formatAmount(
-						row.value.normalizedBigAmount?.normalized || 0,
-						6,
-						configuration.tokenToSend?.decimals || 18
-					)}
-				</span>
-				{` ${configuration.tokenToSend?.symbol || 'Tokens'} to `}
-				<br />
-				<span className={'font-number inline-flex'}>
-					{toAddress(row.receiver.label) === ZERO_ADDRESS ? (
-						<div className={'font-number'}>
-							<span className={'font-bold'}>{row.receiver.label}</span>
-							<span className={'text-xxs'}>{` (${toAddress(row.receiver.address)})`}</span>
-						</div>
-					) : (
-						<div className={'font-number'}>
-							<span className={'font-bold'}>{toAddress(row.receiver.address)}</span>
-						</div>
-					)}
-				</span>
-			</div>
-		</div>
-	);
-}
-
-type TSpendingStatusProps = {
-	disperseStatus: TTxStatus;
-};
-function DisperseStatus({disperseStatus}: TSpendingStatusProps): ReactElement {
-	const {isDispersed, configuration} = useDisperse();
-
-	const validReceivers = useMemo((): TDisperseInput[] => {
-		return configuration.inputs.filter(
-			(row): boolean =>
-				toBigInt(row.value.normalizedBigAmount.raw) !== 0n && row.receiver.address !== ZERO_ADDRESS
-		);
-	}, [configuration.inputs]);
-
-	function renderStatusIndicator(): ReactElement {
-		if (isDispersed) {
-			return <IconCircleCheck className={'size-4 text-green'} />;
-		}
-		if (disperseStatus.pending) {
-			return <IconSpinner className={'size-4'} />;
-		}
-		if (disperseStatus.success) {
-			return <IconCircleCheck className={'size-4 text-green'} />;
-		}
-		if (disperseStatus.error) {
-			return <IconCircleCross className={'size-4 text-red'} />;
-		}
-		return <div className={'size-4 rounded-full border border-neutral-200 bg-neutral-300'} />;
-	}
-
-	return (
-		<div className={'flex flex-col'}>
-			<div className={'flex gap-2 items-center'}>
-				<div className={'font-bold'}>{'Disperse Summary'}</div>
-				{renderStatusIndicator()}
-			</div>
-			<div>
-				{validReceivers.length === 0 ? (
-					<NothingToDisperse />
-				) : (
-					validReceivers.map(
-						(row): ReactElement => (
-							<DisperseElement
-								key={row.UUID}
-								row={row}
-							/>
-						)
-					)
-				)}
-			</div>
-		</div>
-	);
-}
-
 export function DisperseWizard(): ReactElement {
 	const {getBalance} = useWallet();
 	const {isWalletSafe} = useWeb3();
@@ -522,7 +272,7 @@ export function DisperseWizard(): ReactElement {
 		return configuration.inputs.reduce((acc, row): bigint => acc + row.value.normalizedBigAmount.raw, 0n);
 	}, [configuration.inputs]);
 
-	const {shouldApprove, approvalStatus, allowance, isApproved, onApproveToken} = useApproveDisperse({
+	const {isApproved, approvalStatus, onApproveToken} = useApproveDisperse({
 		onSuccess: () => {
 			set_disperseStatus(defaultTxStatus);
 		},
@@ -589,28 +339,27 @@ export function DisperseWizard(): ReactElement {
 		return 'Approve';
 	};
 
+	const getTotalToDisperseLabel = (): string => {
+		if (totalToDisperse) {
+			return `Total to Disperse: ${formatAmount(
+				toNormalizedValue(totalToDisperse, configuration.tokenToSend?.decimals || 18),
+				6,
+				configuration.tokenToSend?.decimals || 18
+			)} ${configuration.tokenToSend?.symbol || 'Tokens'}`;
+		}
+		return 'Nothing to Disperse yet';
+	};
+
 	return (
 		<div className={'col-span-12 mt-4'}>
 			{/* <small className={'pb-1 pl-1'}>{'Summary'}</small> */}
-			<div
-				className={
-					'grid grid-cols-3 md:flex-row gap border gap-12 rounded-lg border-neutral-600 text-neutral-700 bg-neutral-100 p-4'
-				}>
-				{shouldApprove && !isWalletSafe && (
-					<div className={'col-span-1'}>
-						<ApprovalStatus
-							approvalStatus={approvalStatus}
-							allowance={toBigInt(allowance)}
-							totalToDisperse={totalToDisperse}
-						/>
-					</div>
-				)}
-				<div className={'col-span-2'}>
-					<DisperseStatus disperseStatus={disperseStatus} />
-				</div>
-			</div>
+			<span
+				suppressHydrationWarning
+				className={'font-bold'}>
+				{getTotalToDisperseLabel()}
+			</span>
 			<Button
-				isBusy={disperseStatus.pending}
+				isBusy={disperseStatus.pending || approvalStatus.pending}
 				isDisabled={isAboveBalance || configuration.inputs.length === 0 || !isValid}
 				onClick={(): void => {
 					if (isWalletSafe) {
@@ -646,7 +395,6 @@ export function DisperseWizard(): ReactElement {
 				ctaLabel={'Close'}
 				isOpen={disperseStatus.error}
 				onClose={(): void => {
-					onResetDisperse();
 					set_disperseStatus(defaultTxStatus);
 				}}
 			/>
